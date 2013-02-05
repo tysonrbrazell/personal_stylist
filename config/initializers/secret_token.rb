4959621216824d308b06d1f199d8e623cbe02ff7
Tyson R Brazell <tysonrbrazell@gmail.com>
2013-02-05 16:06:19 -0500

    Created a rails application and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalStylist::Application.config.secret_token = 'a9bdfe6885bc09ea5a9bc0ca3b4efdf85004fc2f8abe304eb8389d1433e5bde3781b262dc9fbddc013894617a98bee867b42e5b948fe961128aada5533737e02'
